524680476bba600be6efc5049ffbbb085fb210b7
planetis-m <b3lievem@yandex.com>
2021-05-31 10:41:50 +0300

    fix declaration, adjust docs

--- fuzztempl.nim
+++ fuzztempl.nim
@@ -1,4 +1,4 @@
-## defined in llvm/projects/compiler-rt/lib/fuzzer/FuzzerInterface.h
+# defined in llvm/projects/compiler-rt/lib/fuzzer/FuzzerInterface.h
 ## NOTE: the libFuzzer interface is thin and in the majority of cases
 ## you should not include this file into your target. In 95% of cases
 ## all you need is to define the procedure `testOneInput` in your file.
@@ -6,7 +6,6 @@
 proc initialize: cint {.exportc: "LLVMFuzzerInitialize".} = discard "to implement"
   ## Optional user-provided initialization procedure.
   ## If provided, this procedure will be called by libFuzzer once at startup.
-  ## It may read and modify argc/argv.
   ## Must return 0.
 
 proc testOneInput(data: openarray[byte]): cint {.
@@ -16,7 +15,7 @@ proc testOneInput(data: openarray[byte]): cint {.
   ## libFuzzer will invoke this procedure *many* times with different inputs.
   ## Must return 0.
 
-proc customMutator(data: openarray[byte], maxLen: int, seed: int64): cint {.
+proc customMutator(data: var openarray[byte], maxLen: int, seed: int64): cint {.
     exportc: "LLVMFuzzerCustomMutator".} = discard "to implement"
   ## Optional user-provided custom mutator.
   ## Mutates raw data in `data` inplace.
@@ -24,14 +23,14 @@ proc customMutator(data: openarray[byte], maxLen: int, seed: int64): cint {.
   ## Given the same `seed` produces the same mutation.
 
 proc customCrossOver(data1: openarray[byte], data2: openarray[byte],
-    res: openarray[byte], maxOutLen: int, seed: int64): int {.
+    res: var openarray[byte], maxOutLen: int, seed: int64): int {.
     exportc: "LLVMFuzzerCustomCrossOver".} = discard "to implement"
   ## Optional user-provided custom cross-over procedure.
-  ## Combines pieces of Data1 & Data2 together into Out.
-  ## Returns the new size, which is not greater than MaxOutSize.
-  ## Should produce the same mutation given the same Seed.
+  ## Combines pieces of `data1` & `data2` together into `res`.
+  ## Returns the new length, which is not greater than `maxOutLen`.
+  ## Should produce the same mutation given the same `seed`.
 
-proc mutate(data: openarray[byte], maxLen: int, seed: int64): int {.
+proc mutate(data: var openarray[byte], maxLen: int, seed: int64): int {.
     exportc: "LLVMFuzzerMutate".}
   ## Experimental, may go away in future.
   ## libFuzzer-provided procedure to be used inside `customMutator`.
