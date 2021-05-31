20eaccf6a2d907026b9c05598e5ece656d606a4b
planetis-m <b3lievem@yandex.com>
2021-05-31 10:33:35 +0300

    simplify interface, it would be better to wrap the c function and call them with toOpenarray, as it would produce range checks on the callsite, failing on integer overflows. We want determinism but in practice thats doubtful to be useful

--- fuzztempl.nim
+++ fuzztempl.nim
@@ -1,40 +1,39 @@
-# defined in llvm/projects/compiler-rt/lib/fuzzer/FuzzerInterface.h
-# NOTE: the libFuzzer interface is thin and in the majority of cases
-# you should not include this file into your target. In 95% of cases
-# all you need is to define the procedure `testOneInput` in your file.
+## defined in llvm/projects/compiler-rt/lib/fuzzer/FuzzerInterface.h
+## NOTE: the libFuzzer interface is thin and in the majority of cases
+## you should not include this file into your target. In 95% of cases
+## all you need is to define the procedure `testOneInput` in your file.
 
 proc initialize: cint {.exportc: "LLVMFuzzerInitialize".} = discard "to implement"
-  # Optional user-provided initialization procedure.
-  # If provided, this procedure will be called by libFuzzer once at startup.
-  # It may read and modify argc/argv.
-  # Must return 0.
+  ## Optional user-provided initialization procedure.
+  ## If provided, this procedure will be called by libFuzzer once at startup.
+  ## It may read and modify argc/argv.
+  ## Must return 0.
 
-proc testOneInput(data: ptr UncheckedArray[byte], len: csizeT): cint {.
+proc testOneInput(data: openarray[byte]): cint {.
     exportc: "LLVMFuzzerTestOneInput".} = discard "to implement"
-  # Mandatory user-provided target procedure.
-  # Executes the code under test with [data, data+len) as the input.
-  # libFuzzer will invoke this procedure *many* times with different inputs.
-  # Must return 0.
+  ## Mandatory user-provided target procedure.
+  ## Executes the code under test with `data` as the input.
+  ## libFuzzer will invoke this procedure *many* times with different inputs.
+  ## Must return 0.
 
-proc customMutator(data: ptr UncheckedArray[byte], len, maxLen: csizeT, seed: cuint): cint {.
+proc customMutator(data: openarray[byte], maxLen: int, seed: int64): cint {.
     exportc: "LLVMFuzzerCustomMutator".} = discard "to implement"
-  # Optional user-provided custom mutator.
-  # Mutates raw data in `[data, data+len)` inplace.
-  # Returns the new length, which is not greater than `maxLen`.
-  # Given the same `seed` produces the same mutation.
+  ## Optional user-provided custom mutator.
+  ## Mutates raw data in `data` inplace.
+  ## Returns the new length, which is not greater than `maxLen`.
+  ## Given the same `seed` produces the same mutation.
 
-proc customCrossOver(data1: ptr UncheckedArray[byte], len1: csizeT,
-    data2: ptr UncheckedArray[byte], len2: csizeT, res: ptr UncheckedArray[byte],
-    maxOutLen: sizeT, seed: cuint): csizeT {.
+proc customCrossOver(data1: openarray[byte], data2: openarray[byte],
+    res: openarray[byte], maxOutLen: int, seed: int64): int {.
     exportc: "LLVMFuzzerCustomCrossOver".} = discard "to implement"
-  # Optional user-provided custom cross-over procedure.
-  # Combines pieces of Data1 & Data2 together into Out.
-  # Returns the new size, which is not greater than MaxOutSize.
-  # Should produce the same mutation given the same Seed.
+  ## Optional user-provided custom cross-over procedure.
+  ## Combines pieces of Data1 & Data2 together into Out.
+  ## Returns the new size, which is not greater than MaxOutSize.
+  ## Should produce the same mutation given the same Seed.
 
-proc mutate(data: ptr UncheckedArray[byte], len, maxLen: csizeT, seed: cuint): csizeT {.
+proc mutate(data: openarray[byte], maxLen: int, seed: int64): int {.
     exportc: "LLVMFuzzerMutate".}
-  # Experimental, may go away in future.
-  # libFuzzer-provided procedure to be used inside `customMutator`.
-  # Mutates raw data in `[data, data+len)` inplace.
-  # Returns the new length, which is not greater than `maxLen`.
+  ## Experimental, may go away in future.
+  ## libFuzzer-provided procedure to be used inside `customMutator`.
+  ## Mutates raw data in `data` inplace.
+  ## Returns the new length, which is not greater than `maxLen`.
