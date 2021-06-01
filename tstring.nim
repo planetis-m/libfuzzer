d6d189dc1451e9f73b2cd79ae3996a93fa2c24c0
planetis-m <b3lievem@yandex.com>
2021-06-01 12:54:32 +0300

    update

--- tstring.nim
+++ tstring.nim
@@ -1,10 +1,22 @@
-import strutils
+# Compare asm with C program for interceptors
 
 proc testOneInput*(data: openarray[byte]): cint {.
     exportc: "LLVMFuzzerTestOneInput".} =
   if data.len >= 7:
     var copy = newString(6)
-    copyMem(cstring(copy), data, copy.len)
-    if rfind(copy, "qwertyuiopasdfghjklzxcvbnm") == 0:
+    copyMem(cstring(copy), cast[cstring](data), copy.len)
+    if copy == "qwerty":
       stderr.write("BINGO\n")
       quit(1)
+
+proc strcmp(a, b: cstring): cint {.noSideEffect,
+    importc, header: "<string.h>".}
+
+#proc testOneInput*(data: openarray[byte]): cint {.
+    #exportc: "LLVMFuzzerTestOneInput".} =
+  #if data.len >= 7:
+    #var copy = newString(6)
+    #copyMem(cstring(copy), cast[cstring](data), copy.len)
+    #if strcmp(cstring(copy), cstring"qwerty") == 0:
+      #stderr.write("BINGO\n")
+      #quit(1)
