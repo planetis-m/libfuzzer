4c77a1d9e1ffe3d255c491f176646b68488226f1
planetis-m <b3lievem@yandex.com>
2021-06-02 13:16:14 +0300

    change back to getting capacity

--- compress.nim
+++ compress.nim
@@ -1,5 +1,15 @@
 import snappy, std/strutils
 
+type
+  SeqPayload = object
+    cap: int
+  SeqHeader = object
+    len: int
+    p: ptr SeqPayload
+
+proc capacity[T](x: seq[T]): int =
+  cast[SeqHeader](x).p[].cap
+
 proc testOneInput(data: openarray[byte]): cint {.
     exportc: "LLVMFuzzerTestOneInput".} =
   # Decompress the input data and crash if it starts with "boom".
@@ -15,12 +25,13 @@ proc customMutator(data: var openarray[byte], maxLen: int, seed: int64): int {.
   var uncompressed = uncompress(data)
   if uncompressed.len == 0: uncompressed = cast[seq[byte]](@"hi")
   # Mutate the uncompressed data with `libFuzzer`'s default mutator. Make
-  # the `uncompressed` seq bigger for inserting mutations via `grow`.
-  let extraLen = uncompressed.len * 2
-  uncompressed.grow(extraLen, 0)
-  let newDecompressedLen = mutate(uncompressed, extraLen)
+  # the `decompressed` seq's extra capacity available for inserting
+  # mutations via `grow`.
+  let cap = uncompressed.capacity
+  uncompressed.grow(cap, 0)
+  let newDecompressedLen = mutate(uncompressed, cap)
   # Recompress the mutated data.
   let compressed = compress(uncompressed.toOpenArray(0, newDecompressedLen-1))
-  # Copy the recompressed mutated data into `data` and return the new size.
+  # Copy the recompressed mutated data into `data` and return the new length.
   result = min(maxLen, compressed.len)
   for i in 0..<result: data[i] = compressed[i]
