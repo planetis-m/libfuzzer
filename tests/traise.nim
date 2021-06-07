a71bcc02a009e031090f72cdd74217fa3075540d
planetis-m <b3lievem@yandex.com>
2021-06-07 23:34:07 +0300

    cmdline example

--- tests/traise.nim
+++ tests/traise.nim
@@ -1,4 +1,6 @@
 
+proc quitOrDebug() {.noreturn, importc: "abort", header: "<stdlib.h>", nodecl.}
+
 proc initialize(): cint {.exportc: "LLVMFuzzerInitialize".} =
   {.emit: "N_CDECL(void, NimMain)(void); NimMain();".}
 
@@ -6,3 +8,10 @@ proc testOneInput(data: ptr UncheckedArray[byte], len: int): cint {.
     exportc: "LLVMFuzzerTestOneInput".} =
   if true:
     raise newException(ValueError, "")
+
+proc customMutator(data: ptr UncheckedArray[byte]; len, maxLen: int, seed: int64): int {.
+    exportc: "LLVMFuzzerCustomMutator".} =
+  try:
+    discard
+  except:
+    quitOrDebug()
